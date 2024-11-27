5cac3f33f1c4b565dfb6f25d7952320ead517e41
Patricia Robles <122781084+PattiRobles@users.noreply.github.com>
2024-11-27 17:16:39 +0000

    provides solution to exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(sentence) {
+  let reversedSentence = ''
+for (let i = sentence.length-1; i >=0; i--) {
+  reversedSentence += sentence[i]
+}
+return reversedSentence
 };
 
 // Do not edit below this line
