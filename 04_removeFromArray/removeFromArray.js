77976b1139ac897f23f1de231905e4dd5eb4fa0d
Patricia Robles <122781084+PattiRobles@users.noreply.github.com>
2024-11-27 17:58:54 +0000

    provides solution to exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+   return array.filter(value => !itemsToRemove.includes(value));
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//rest parameter allows a fx to accept indefinite number of arguments as an array
