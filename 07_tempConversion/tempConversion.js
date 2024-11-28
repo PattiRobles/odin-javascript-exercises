b57dd65451eddc246b8b6436cd11946125610194
Patricia Robles <122781084+PattiRobles@users.noreply.github.com>
2024-11-28 16:21:32 +0000

    provides solution to exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+ let ftoC = (F-32)/1.8
+ return +ftoC.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let ctoF = (C*1.8) + 32
+  return +ctoF.toFixed(1)
 };
+
 
 // Do not edit below this line
 module.exports = {
