d302c0d0a62e7cc339bc29b54b90d899cad89aec
Patricia Robles <122781084+PattiRobles@users.noreply.github.com>
2024-11-28 15:20:28 +0000

    provides solution to exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
 
+  if (typeof min !== 'number' || typeof max !== 'number') return 'ERROR'
+  if (min % 1 != 0 || max % 1 != 0) return 'ERROR'
+  //method .isInteger would have covered type number and decimal
+  //if(!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR'
+  if (min < 0 || max < 0) return 'ERROR'
+
+  if (min > max) [min, max] = [max, min]
+  for (let i = lower; i <= upper; i++) {
+   sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
